8d165f20d7c12be5b7957cf61d4f466343334b35
Daniel Langley <dlangley812@hotmail.com>
2026-04-16 00:24:15 -0500

    Put in skeleton of contact list

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import (
     Flask,
+    redirect,
+    render_template,
+    url_for,
 )
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'index'
+    # return render_template('index.html')
+    return redirect(url_for('get_contacts'))
+
+@app.route('/contacts')
+def get_contacts():
+    contacts = ['a', 'b', 'c']
+    return render_template('contacts.html',
+                           contacts=contacts)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5003)
