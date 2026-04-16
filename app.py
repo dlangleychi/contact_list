f241e8b65dae9241cc3981ef32c49bbfa7d94429
Daniel Langley <dlangley812@hotmail.com>
2026-04-16 12:41:11 -0500

    Add new contact form

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ from flask import (
     Flask,
     redirect,
     render_template,
+    session,
     url_for,
 )
 
 app = Flask(__name__)
+app.secret_key = 'secret'
+
+@app.before_request
+def initialize_session():
+    if 'contacts' not in session:
+        session['contacts'] = ['apple', 'banana', 'corn']
 
 @app.route('/')
 def index():
@@ -14,9 +21,17 @@ def index():
 
 @app.route('/contacts')
 def get_contacts():
-    contacts = ['a', 'b', 'c']
+    contacts = session['contacts']
     return render_template('contacts.html',
                            contacts=contacts)
+
+@app.route('/contacts/new')
+def add_contact():
+    return render_template('new_contact.html')
+
+@app.route('/contacts', methods=['POST'])
+def create_contact():
+    return redirect(url_for('get_contacts'))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5003)
